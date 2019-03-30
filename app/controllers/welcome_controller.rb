8d866b5500f6c97032af1f9d9abcb4406212ae6c
Sujan Abraham <itssujan@gmail.com>
2019-03-30 09:28:05 -0400

    use model in controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,7 @@
 class WelcomeController < ApplicationController
-  CRONUTS_URL = 'https://upload.wikimedia.org/wikipedia/commons/8/8b/April_2016_Cronut%C2%AE_2_Burnt_Vanilla_'\
-  'Caramel_-_photo_by_Dominique_Ansel_Bakery.jpg'.freeze
-  DONUTS_URL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/250px-Glazed-Donut'\
-  '.jpg'.freeze
-
   def index
-    @cronut_url = params[:cronuts_url] || CRONUTS_URL
-    @donut_url  = params[:donuts_url] || DONUTS_URL
+    widget = Widget.new(params[:donuts_url], params[:cronuts_url])
+    @cronut_url = widget.cronuts_url
+    @donut_url  = widget.donuts_url
   end
 end
