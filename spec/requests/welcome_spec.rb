a714998babf4eb28872219c1281830ebeba17703
Sujan Abraham <itssujan@gmail.com>
2019-03-30 09:36:35 -0400

    refactoring welcome spec

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -14,30 +14,24 @@ RSpec.describe WelcomeController, type: :request do
       end
 
       it 'expects page to contain widget text' do
-        widget_text = "By selecting this checkbox, you agree that <a href=\"#{CRONUTS_URL}\" target=\"_blank\">"\
-                      "cronuts</a> are better than <a href=\"#{DONUTS_URL}\" target=\"_blank\">donuts</a>"
         get '/welcome/index'
-        expect(response.body).to include(widget_text)
+        expect(response.body).to include widget_text
       end
     end
 
     context 'when index is called with a cronut url param' do
       it 'should change cronut url' do
         cronuts_url_param = 'https://google.com'
-        widget_text = "By selecting this checkbox, you agree that <a href=\"#{cronuts_url_param}\" target=\"_blank\">"\
-                      "cronuts</a> are better than <a href=\"#{DONUTS_URL}\" target=\"_blank\">donuts</a>"
         get '/welcome/index', params: { cronuts_url: cronuts_url_param }
-        expect(response.body).to include(widget_text)
+        expect(response.body).to include widget_text(cronuts_url: cronuts_url_param)
       end
     end
 
     context 'when index is called with a donut url param' do
       it 'should change donut url' do
         donut_url_param = 'https://google.com'
-        widget_text = "By selecting this checkbox, you agree that <a href=\"#{CRONUTS_URL}\" target=\"_blank\">"\
-                      "cronuts</a> are better than <a href=\"#{donut_url_param}\" target=\"_blank\">donuts</a>"
         get '/welcome/index', params: { donuts_url: donut_url_param }
-        expect(response.body).to include(widget_text)
+        expect(response.body).to include widget_text(donut_url: donut_url_param)
       end
     end
 
@@ -45,12 +39,16 @@ RSpec.describe WelcomeController, type: :request do
       it 'should change both donut and cronut urls' do
         donut_url_param = 'https://google.com'
         cronuts_url_param = 'https://google1.com'
-        widget_text = "By selecting this checkbox, you agree that <a href=\"#{cronuts_url_param}\" target=\"_blank\">"\
-                      "cronuts</a> are better than <a href=\"#{donut_url_param}\" target=\"_blank\">donuts</a>"
         get '/welcome/index', params: { donuts_url: donut_url_param, cronuts_url: cronuts_url_param }
-        expect(response.body).to include(widget_text)
+        expect(response.body).to include widget_text(cronuts_url: cronuts_url_param, donut_url: donut_url_param)
       end
     end
+  end
 
+  private
+  
+  def widget_text(donut_url: DONUTS_URL, cronuts_url: CRONUTS_URL)
+    "By selecting this checkbox, you agree that <a href=\"#{cronuts_url}\" target=\"_blank\">"\
+    "cronuts</a> are better than <a href=\"#{donut_url}\" target=\"_blank\">donuts</a>"
   end
 end
