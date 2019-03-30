3588ec73dcf27f73424b1f07191979e03ab1d5ca
Sujan Abraham <itssujan@gmail.com>
2019-03-30 08:39:55 -0400

    pull out cronut and donut urls to controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+
+  CRONUTS_URL = 'https://upload.wikimedia.org/wikipedia/commons/8/8b/April_2016_Cronut%C2%AE_2_Burnt_Vanilla_'\
+  'Caramel_-_photo_by_Dominique_Ansel_Bakery.jpg'.freeze
+  DONUTS_URL  = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/250px-Glazed-Donut'\
+  '.jpg'.freeze
+
   def index
+    @cronut_url = CRONUTS_URL
+    @donut_url  = DONUTS_URL
   end
 end
