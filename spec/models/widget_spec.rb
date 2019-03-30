d0951c6de43464b0f8d2169609ff3738ac37d6d5
Sujan Abraham <itssujan@gmail.com>
2019-03-30 09:54:55 -0400

    rubocop

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Widget do
   CRONUTS_URL = 'https://upload.wikimedia.org/wikipedia/commons/8/8b/April_2016_Cronut%C2%AE_2_Burnt_Vanilla_'\
   'Caramel_-_photo_by_Dominique_Ansel_Bakery.jpg'.freeze
-  DONUTS_URL  = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/250px-Glazed-Donut'\
+  DONUTS_URL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/250px-Glazed-Donut'\
   '.jpg'.freeze
 
   describe '.initialize' do
--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :request do
-  CRONUTS_URL = 'https://upload.wikimedia.org/wikipedia/commons/8/8b/April_2016_Cronut%C2%AE_2_Burnt_Vanilla_'\
-                'Caramel_-_photo_by_Dominique_Ansel_Bakery.jpg'.freeze
-  DONUTS_URL  = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/250px-Glazed-Donut'\
-                '.jpg'.freeze
+  CRONUTS_DEFAULT_URL = 'https://upload.wikimedia.org/wikipedia/commons/8/8b/April_2016_Cronut%C2%AE_2_'\
+                         'Burnt_Vanilla_Caramel_-_photo_by_Dominique_Ansel_Bakery.jpg'.freeze
+  DONUTS_DEFAULT_URL  = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/'\
+                        '250px-Glazed-Donut.jpg'.freeze
 
   describe '#index' do
     context 'when index is called' do
@@ -47,7 +47,7 @@ RSpec.describe WelcomeController, type: :request do
 
   private
 
-  def widget_text(donut_url: DONUTS_URL, cronuts_url: CRONUTS_URL)
+  def widget_text(donut_url: DONUTS_DEFAULT_URL, cronuts_url: CRONUTS_DEFAULT_URL)
     I18n.t('welcome.index.super_terms_html', cronuts_link: cronuts_url, donuts_link: donut_url)
   end
 end
